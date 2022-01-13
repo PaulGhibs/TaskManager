f7c6759535140052252bfbb788da585ad18b0fbe
Paul Ghibs <paul.ghib@gmail.com>
2022-01-13 19:05:12 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Paul Ghibeaux on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
